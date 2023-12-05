42cb32ce57e60076c87c38969e0824baeb900168
nikolas nakamura <nikolas.shiro.nakamura@gmail.com>
2023-12-05 22:03:20 +0900

    test of prompt-sync

---
+++ project.js
@@ -0,0 +1,15 @@
+// 1. Despot some money
+// 2. Determine number of lines to bet on
+// 3. collect a bet amount
+// 4. Spin the slot machine
+// 5. check if the user won
+// 6. give the user their winning
+// 7. play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+  const depositAmount = prompt("Enter a deposit amount: ");
+};
+
+deposit();
